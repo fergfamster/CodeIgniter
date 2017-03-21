b1629ec2792a53086c4683f0f6b034711e94c04a
James Ferguson <fergfamster@gmail.com>
2017-03-21 14:19:46 -0700

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter for your new site</title>
+	<title>Welcome to CodeIgniter for James</title>
 
 	<style type="text/css">
 
